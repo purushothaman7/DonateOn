080bf632865567b997e394f0fff3ef27b8bc6f58
sharvesh <sharveshr03@gmail.com>
2023-09-05 23:32:14 +0530

    registration validation

--- django_donations/donate_app/models.py
+++ django_donations/donate_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Signup(models.Model):
+    # CITY_CHOICES = (
+    #     (Chennai , "Chennai"),
+    #     (Kanchipuram, "Kanchipuram"),
+    #     (Thiruvallur, "Thiruvallur"),
+    # )
+    username = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=10)
+    email = models.EmailField(max_length=254)
+    address = models.TextField()
+    # city = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+    # choices = CITY_CHOICES, default = Chennai
+
+    
--- django_donations/donate_app/views.py
+++ django_donations/donate_app/views.py
@@ -1,28 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Signup
+from django.contrib.auth.models import auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-<<<<<<< HEAD
     return render(request, 'index.html')
 
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
+        phone_no = request.POST['phone']
         email = request.POST['email']
+        address = request.POST['address']
+        # city = request.POST['Select_City']
         password = request.POST['password']
         password2 = request.POST['repeatpassword']
         
         if password == password2:
-            if User.objects.filter(email=email).exists():
+            if Signup.objects.filter(email=email).exists():
                 messages.info(request,'Email already exists')
                 return redirect('register')
             
-            elif User.objects.filter(username=username).exists():
+            elif Signup.objects.filter(username=username).exists():
                 messages.info(request,'Username already exists')
                 return redirect('register')
             
+            elif Signup.objects.filter(phone_no=phone_no).exists():
+                messages.info(request,'Phone number already exists')
+                return redirect('register')
+            
+    
+            
             else:
-                user = User.objects.create_user(username=username,email=email,password=password) 
+                user = Signup(username=username,email=email,password=password,phone_no=phone_no,address=address) 
                 user.save()
                 return redirect('/')
         
@@ -55,6 +66,3 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect("/")
-=======
-    return render(request, 'index.html')
->>>>>>> 87160d9072b7f5b70882e5cd6c09a53e84a50f53
